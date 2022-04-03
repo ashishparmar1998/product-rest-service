271223ffc29ac8bcaeea64c205a94abbfe5d708b
ashishparmar1998 <ashishparma68@gmail.com>
2022-04-03 12:36:11 +0530

    initial commit

---
+++ src/main/java/com/assignment/productrestservice/ProductRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.productrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/productrestservice/ProductRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.productrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
